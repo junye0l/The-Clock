e08acab3c944809d7feb544ac07680888fc0b65e
junye0l <junyeolkim00@gmail.com>
2025-08-19 22:11:43 +0900

    docs : 프로젝트 기초 설정

---
+++ TheClock/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="text-white text-6xl font-mono">
+        {/* 여기에 시계 컴포넌트를 추가하세요 */}
+        00:00:00
+      </div>
+    </div>
+  );
+}
+
+export default App;
